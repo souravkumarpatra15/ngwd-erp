fff34455d6f4bc5d5fd736d1a2b2ab4d8a8948b5
Sourav Kumar Patra <97288696+souravkumarpatra15@users.noreply.github.com>
2026-08-23 09:38:10 +0530

    refactor: normalize document model formatting

--- app/Models/DocumentModel.php
+++ app/Models/DocumentModel.php
@@ -1,9 +1,27 @@
 <?php
+
 namespace App\Models;
+
 use CodeIgniter\Model;
-class DocumentModel extends Model {
+
+class DocumentModel extends Model
+{
     protected $table = 'documents';
     protected $primaryKey = 'id';
     protected $useTimestamps = true;
-    protected $allowedFields = ['client_id','project_id','category','title','file_name','file_path','file_size','file_type','reference_id','reference_type','notes','created_by'];
+
+    protected $allowedFields = [
+        'client_id',
+        'project_id',
+        'category',
+        'title',
+        'file_name',
+        'file_path',
+        'file_size',
+        'file_type',
+        'reference_id',
+        'reference_type',
+        'notes',
+        'created_by',
+    ];
 }
