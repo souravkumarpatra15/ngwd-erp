d3810ce9f93d01d867241724d1ddcc03e31eef68
Sourav Kumar Patra <97288696+souravkumarpatra15@users.noreply.github.com>
2026-08-23 12:18:04 +0530

    security: force HTTPS requests in production

--- app/Config/App.php
+++ app/Config/App.php
@@ -6,123 +6,20 @@ use CodeIgniter\Config\BaseConfig;
 
 class App extends BaseConfig
 {
-    /**
-     * --------------------------------------------------------------------------
-     * Base Site URL
-     * --------------------------------------------------------------------------
-     *
-     * URL to your CodeIgniter root. Typically, this will be your base URL,
-     * WITH a trailing slash:
-     *
-     * E.g., http://example.com/
-     */
     public string $baseURL = 'http://localhost:8080/';
-
-    /**
-     * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
-     * If you want to accept multiple Hostnames, set this.
-     *
-     * E.g.,
-     * When your site URL ($baseURL) is 'http://example.com/', and your site
-     * also accepts 'http://media.example.com/' and 'http://accounts.example.com/':
-     *     ['media.example.com', 'accounts.example.com']
-     *
-     * @var list<string>
-     */
     public array $allowedHostnames = [];
-
-    /**
-     * --------------------------------------------------------------------------
-     * Index File
-     * --------------------------------------------------------------------------
-     *
-     * Typically, this will be your `index.php` file, unless you've renamed it to
-     * something else. If you have configured your web server to remove this file
-     * from your site URIs, set this variable to an empty string.
-     */
     public string $indexPage = '';
-
-    /**
-     * --------------------------------------------------------------------------
-     * URI PROTOCOL
-     * --------------------------------------------------------------------------
-     *
-     * This item determines which server global should be used to retrieve the
-     * URI string. The default setting of 'REQUEST_URI' works for most servers.
-     *
-     * WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
-     */
     public string $uriProtocol = 'REQUEST_URI';
-
-    /*
-    |--------------------------------------------------------------------------
-    | Allowed URL Characters
-    |--------------------------------------------------------------------------
-    */
     public string $permittedURIChars = 'a-z 0-9~%.:_\-';
-
-    /**
-     * --------------------------------------------------------------------------
-     * Default Locale
-     * --------------------------------------------------------------------------
-     */
     public string $defaultLocale = 'en';
-
-    /**
-     * --------------------------------------------------------------------------
-     * Negotiate Locale
-     * --------------------------------------------------------------------------
-     */
     public bool $negotiateLocale = false;
-
-    /**
-     * --------------------------------------------------------------------------
-     * Supported Locales
-     * --------------------------------------------------------------------------
-     *
-     * @var list<string>
-     */
     public array $supportedLocales = ['en'];
-
-    /**
-     * --------------------------------------------------------------------------
-     * Application Timezone
-     * --------------------------------------------------------------------------
-     *
-     * NGWebD business operations are currently India-based. Keeping the
-     * application timezone aligned with the business timezone ensures that
-     * invoices, payments, follow-ups, renewals, audit logs and reminders use
-     * consistent local dates and times.
-     */
     public string $appTimezone = 'Asia/Kolkata';
-
-    /**
-     * --------------------------------------------------------------------------
-     * Default Character Set
-     * --------------------------------------------------------------------------
-     */
     public string $charset = 'UTF-8';
 
-    /**
-     * --------------------------------------------------------------------------
-     * Force Global Secure Requests
-     * --------------------------------------------------------------------------
-     */
-    public bool $forceGlobalSecureRequests = false;
+    // Production should only serve authenticated/business traffic over HTTPS.
+    public bool $forceGlobalSecureRequests = ENVIRONMENT === 'production';
 
-    /**
-     * --------------------------------------------------------------------------
-     * Reverse Proxy IPs
-     * --------------------------------------------------------------------------
-     *
-     * @var array<string, string>
-     */
     public array $proxyIPs = [];
-
-    /**
-     * --------------------------------------------------------------------------
-     * Content Security Policy
-     * --------------------------------------------------------------------------
-     */
     public bool $CSPEnabled = false;
 }
